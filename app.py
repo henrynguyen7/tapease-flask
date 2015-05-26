1b1d0e2723cf522e79ba10e50234f9c778f0f46d
Henry Nguyen <henry@dxconcept.com>
2015-05-25 21:10:40 -0700

    Added Users model and endpoint. Added support for page_token in addition to base_url route determination.

--- app.py
+++ app.py
@@ -7,26 +7,53 @@ from model import db
 
 app = Flask(__name__)
 
+
+@app.route("/user", methods=['POST'])
+def user():
+    if request.method == 'POST':
+        user = User(
+            email=request.get_json().get('email'),
+            password=request.get_json().get('password'),
+            name=request.get_json().get('name'),
+            image_url=request.get_json().get('image_url'),
+        )
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(user.serialize)
+
+@app.route("/auth", methods=['POST'])
+def auth():
+    if request.method == 'POST':
+        pass
+
 @app.route("/tap", methods=['GET', 'POST'])
 def tap():
     if request.method == 'POST':
         tap = Tap(
+            user_id=request.get_json().get('user_id'),
+            page_token=request.get_json().get('page_token'),
             base_url=request.get_json().get('base_url'),
-            params=request.get_json().get('params'),
+            route_params=request.get_json().get('route_params'),
+            query_params=request.get_json().get('query_params'),
             element_route=request.get_json().get('element_route'),
-            username=request.get_json().get('username'),
-            image_url=request.get_json().get('image_url'),
+            node=request.get_json().get('node'),
             comment=request.get_json().get('comment'),
-            date=request.get_json().get('date', None),
+            create_date=request.get_json().get('create_date', None),
         )
         db.session.add(tap)
         db.session.commit()
         return jsonify(tap.serialize)
     else:
-        results = Tap.query \
-            .filter_by(base_url=request.args.get('base_url', '')) \
-            .order_by(Tap.date) \
-            .all()
+        if request.args.get('page_token'):
+            results = Tap.query \
+                .filter_by(page_token=request.args.get('page_token', '')) \
+                .order_by(Tap.create_date) \
+                .all()
+        elif request.args.get('base_url'):
+            results = Tap.query \
+                .filter_by(base_url=request.args.get('base_url', '')) \
+                .order_by(Tap.create_date) \
+                .all()
         return jsonify(taps=[t.serialize for t in results])
 
 @app.route("/createdb", methods=['GET'])
--- model.py
+++ model.py
@@ -6,39 +6,86 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tap.db'
 db = SQLAlchemy(app)
 
-class Tap(db.Model):
+secret_key = 'tapeasebaby'
 
-    __tablename__ = 'tap'
+
+class User(db.Model):
+
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    base_url = db.Column(db.String(80))
-    params = db.Column(db.String(80))
-    element_route = db.Column(db.String(80))
-    username = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    name = db.Column(db.String(80))
     image_url = db.Column(db.String(80))
-    comment = db.Column(db.Text)
-    date = db.Column(db.DateTime)
+    is_enabled = db.Column(db.Boolean)
+    create_date = db.Column(db.DateTime)
+    access_token = db.Column(EncryptedType(db.String, secret_key))
 
-    def __init__(self, base_url, base_url, element_route, username, comment, image_url="", date=None):
-        self.base_url = base_url
-        self.params = params
-        self.element_route = element_route
-        self.username = username
-        self.comment = comment
+    def __init__(self, email, password, name, image_url="", is_enabled=True, create_date=None):
+        self.email = email
+        self.password = password
+        self.name = name
         self.image_url = image_url
-        if date is None:
-            date = datetime.utcnow()
-        self.date = date
+        self.is_enabled = is_enabled
+        if create_date is None:
+            create_date = datetime.utcnow()
+        self.create_date = create_date
 
     @property
     def serialize(self):
        return {
            'id': self.id,
-           'base_url': self.base_url,
-           'params': self.params,
-           'element_route': self.element_route,
-           'username': self.username,
+           'email': self.email,
+           'password': self.password,
+           'name': self.name,
            'image_url': self.image_url,
+           'is_enabled': self.is_enabled,
+           'create_date': self.create_date.isoformat(),
+       }
+
+
+class Tap(db.Model):
+
+    __tablename__ = 'tap'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(Integer, ForeignKey('user.id'))
+    page_token = db.Column(db.String(80))
+    base_url = db.Column(db.String(80))
+    route_params = db.Column(db.String(80))
+    query_params = db.Column(db.String(80))
+    element_route = db.Column(db.String(80))
+    node = db.Column(db.String(300))
+    comment = db.Column(db.Text)
+    create_date = db.Column(db.DateTime)
+
+    def __init__(self, user_id, element_route, comment, page_token=None, base_url=None, route_params=None, query_params=None, create_date=None):
+        self.user_id = user_id
+        if page_token is not None:
+            self.page_token = page_token
+        elif base_url is not None:
+            self.base_url = base_url
+            self.route_params = route_params
+            self.query_params = query_params
+        self.element_route = element_route
+        self.node = node
+        self.comment = comment
+        if create_date is None:
+            create_date = datetime.utcnow()
+        self.create_date = create_date
+
+    @property
+    def serialize(self):
+       return {
+           'id': self.id,
+           'user_id': self.user_id,
+           'page_token': self.page_token,
+           'base_url': self.base_url,
+           'route_params': self.route_params,
+           'query_params': self.query_params,
+           'element_route': self.element_route,
+           'node': self.node,
            'comment': self.comment,
-           'date': self.date.isoformat(),
+           'create_date': self.create_date.isoformat(),
        }
